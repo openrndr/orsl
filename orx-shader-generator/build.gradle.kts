165b260d19a597410b526e8868cb6c1cdf6d773a
Edwin Jakobs <edwin@rndr.studio>
2023-06-15 11:12:37 +0200

    Upgrade to Gradle 8.1.1, Kotlin 1.8.22, Kotlinx coroutines 1.7.1

--- orx-shader-generator/build.gradle.kts
+++ orx-shader-generator/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
 
 plugins {
     org.openrndr.extra.convention.`kotlin-multiplatform`
-    id("com.google.devtools.ksp") version "1.8.21-1.0.11"
+    id("com.google.devtools.ksp") version "1.8.22-1.0.11"
 
 }
 
