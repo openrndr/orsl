02579558e45f86aee79884ffa61a5fb69474fb2f
Edwin Jakobs <edwin@rndr.studio>
2023-11-01 19:04:06 +0100

    Sync gradle configuration with orx

--- buildSrc/src/main/kotlin/org/openrndr/extra/convention/kotlin-multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/org/openrndr/extra/convention/kotlin-multiplatform.gradle.kts
@@ -154,3 +154,12 @@ if (shouldPublish) {
         sign(publishing.publications)
     }
 }
+
+kotlin {
+    jvm().mainRun {
+        classpath(kotlin.jvm().compilations.getByName("demo").output.allOutputs)
+        classpath(kotlin.jvm().compilations.getByName("demo").configurations.runtimeDependencyConfiguration!!)
+    }
+}
+
+tasks.withType<JavaExec>().matching { it.name == "jvmRun" }.configureEach { workingDir = rootDir }
