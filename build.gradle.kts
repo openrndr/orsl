f1e07c1aa0f3dac1b409e55fc6d16ce0684b5cfd
Edwin Jakobs <edwin@rndr.studio>
2024-01-13 16:16:28 +0100

    Add gradle nexus publish configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,20 @@
 group = "org.openrndr.orsl"
 plugins {
     alias(libs.plugins.nebula.release)
+    alias(libs.plugins.gradle.nexus.publish)
 }
+
 repositories {
     mavenCentral()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set((findProperty("ossrhUsername") as String?) ?: System.getenv("OSSRH_USERNAME"))
+            password.set((findProperty("ossrhPassword") as String?) ?: System.getenv("OSSRH_PASSWORD"))
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots"))
+        }
+    }
 }
